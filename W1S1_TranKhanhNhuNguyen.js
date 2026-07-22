27a08b9822078cb6ed46e3d5d7ea5d6181b0f0db
nhuntkn <nhuntkn@gmail.com>
2026-07-22 22:32:49 +0700

    Add basic cart caculation function

---
+++ W1S1_TranKhanhNhuNguyen.js
@@ -0,0 +1,10 @@
+//function to calculate the cart price if all the items have the same price
+function calculateCart(itemPrice, quantity, isMember) {
+
+
+}
+
+//function to calculate the cart price if all the items have different prices
+function calculateCartDiffPrice(items, isMember) {
+
+}
